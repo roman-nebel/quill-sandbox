95339323047ded5b29e7800c397669157c6bf383
Roman Nebel <im.roman.nebel@gmail.com>
2024-03-16 19:12:17 +0100

    Start the experiment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactQuill, { Quill } from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 function App() {
+  const [editorValue, setEditorValue] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ReactQuill
+        value={editorValue}
+        modules={{
+          toolbar: [
+            ["bold", "italic"],
+            [{ list: "ordered" }, { list: "bullet" }, "task"],
+            [
+              "header",
+              "blockquote",
+              "code-block",
+              // 'code' //need to handle both in a smart way
+            ],
+            ["link"],
+            [
+              {
+                background: [],
+              },
+            ],
+          ],
+        }}
+        onChange={(value) => {
+          setEditorValue(value);
+        }}
+      />
+      <ReactQuill
+        value={editorValue}
+        modules={{
+          toolbar: [
+            ["bold", "italic"],
+            [{ list: "ordered" }, { list: "bullet" }, "task"],
+            [
+              "header",
+              "blockquote",
+              "code-block",
+              // 'code' //need to handle both in a smart way
+            ],
+            ["link"],
+            [
+              {
+                background: [
+                  "",
+                  "rgba(220, 131, 131, 0.4)",
+                  "rgba(176, 161, 73, 0.4)",
+                  "rgba(122, 177, 110, 0.4)",
+                  "rgba(104, 164, 226, 0.4)",
+                  "rgba(183, 139, 209, 0.4)",
+                  "rgba(1,1,1,0.2)",
+                ],
+              },
+            ],
+          ],
+        }}
+        onChange={(value) => {
+          setEditorValue(value);
+        }}
+      />
+    </>
   );
 }
 
